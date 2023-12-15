fdf42500759da81e3e3033c4b47bae01df0d9193
Frans Bouma <frans@sd.nl>
2023-12-15 12:00:03 +0100

    13 part 1

---
+++ src/AoC2023.Code/Day13.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using SD.Tools.Algorithmia.GeneralDataStructures;
+using SD.Tools.BCLExtensions.CollectionsRelated;
+
+namespace AoC2023.Core
+{
+	public static class Day13
+	{
+		public class Patterns
+		{
+			public Patterns(List<string> input)
+			{
+				List<string> patternContent = new();
+				foreach(var s in input)
+				{
+					if(string.IsNullOrWhiteSpace(s))
+					{
+						PatternContents.Add(patternContent);
+						patternContent = new List<string>();
+					}
+					else
+					{
+						patternContent.Add(s);
+					}
+				}
+				PatternContents.Add(patternContent);
+			}
+
+
+			public long CalculateValue()
+			{
+				long toReturn = 0;
+				foreach(var p in PatternContents)
+				{
+					toReturn += CalculateHorizontalReflectionValue(p) * 100;
+					toReturn += CalculateVerticalReflectionValue(p);
+				}
+				return toReturn;
+			}
+
+
+			private long CalculateVerticalReflectionValue(List<string> p)
+			{
+				// rotate 90 degrees to the right, starting at the bottom left corner
+				var rotatedP = new List<string>();
+				var copiedP = p.ToList();
+				copiedP.Reverse();
+				for(int i = 0; i < p[0].Length; i++)
+				{
+					rotatedP.Add(string.Join("", (from s in copiedP select s[i]).ToArray()));
+				}
+				
+				return CalculateHorizontalReflectionValue(rotatedP);
+			}
+
+
+			private long CalculateHorizontalReflectionValue(List<string> p)
+			{
+				int rowBeforeReflectionLine = 0;
+				for(int i = 0; i < p.Count-1; i++)
+				{
+					if(p[i + 1] == p[i])
+					{
+						// check reflection line
+						bool reflectionOk = true;
+						int delta = 1;
+						while(reflectionOk)
+						{
+							if(i - delta < 0 || i + delta + 1 >= p.Count)
+							{
+								// done
+								break;
+							}
+
+							reflectionOk &= (p[i - delta] == p[i + delta + 1]);
+							delta++;
+						}
+						if(reflectionOk)
+						{
+							rowBeforeReflectionLine = i+1;
+							break;
+						}
+					}
+				}
+
+				return rowBeforeReflectionLine;
+			}
+
+			public List<List<string>> PatternContents { get; } = new();
+		}
+		
+		
+		public static long Solve1(List<string> input)
+		{
+			var p = new Patterns(input);
+			return p.CalculateValue();
+		}
+
+
+		public static long Solve2(List<string> input)
+		{
+			return -1;
+		}
+	}
+}
---
+++ src/AoC2023.Tests/Day13Tests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using AoC2023.Core;
+using NUnit.Framework;
+
+namespace AoC2023.Tests
+{
+	public class Day13Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+
+		[Test]
+		public void Puzzle1_ExampleInput()
+		{
+			var input = InputReader.GetInputAsStringList("..\\..\\..\\PuzzleInputs\\day13_example.txt");
+			Assert.IsTrue(input.Count>0);
+			Assert.AreEqual(405, Day13.Solve1(input));
+		}
+
+		[Test]
+		public void Puzzle1_Solver()
+		{
+			Stopwatch sw = new();
+			sw.Start();
+			var input = InputReader.GetInputAsStringList("..\\..\\..\\PuzzleInputs\\day13.txt");
+			Assert.IsTrue(input.Count>0);
+			Console.WriteLine(Day13.Solve1(input));
+			sw.Stop();
+			Console.WriteLine("Total time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+		}
+		
+		
+		[Test]
+		public void Puzzle2_ExampleInput()
+		{
+			var input = InputReader.GetInputAsStringList("..\\..\\..\\PuzzleInputs\\day13_example.txt");
+			Assert.IsTrue(input.Count>0);
+			Assert.AreEqual(400, Day13.Solve2(input));
+		}
+		
+		
+		[Test]
+		public void Puzzle2_Solver()
+		{
+			Stopwatch sw = new();
+			sw.Start();
+			var input = InputReader.GetInputAsStringList("..\\..\\..\\PuzzleInputs\\day13.txt");
+			Assert.IsTrue(input.Count>0);
+			Console.WriteLine(Day13.Solve2(input));
+			sw.Stop();
+			Console.WriteLine("Total time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+		}
+	}
+}
